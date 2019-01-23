ab00ee62b927461a6d8c49a822ba03a90db89f47
Aru <arundhati.bhende@mckesson.com>
2019-01-22 21:12:57 -0500

    Initial commit

---
+++ src/main/java/com/test/okta/oidcapp/oktaoidcspring21/ExampleApplication.java
@@ -0,0 +1,37 @@
+package com.test.okta.oidcapp.oktaoidcspring21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@SpringBootApplication
+public class ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class, args);
+    }
+
+    @GetMapping("/hello-oauth")
+    public String sayHello(Principal principal) {
+        return "Hello, " + principal.getName();
+    }
+
+    @Configuration
+    static class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests().anyRequest().authenticated()
+                    .and()
+                    .oauth2ResourceServer().jwt();
+        }
+    }
+}
